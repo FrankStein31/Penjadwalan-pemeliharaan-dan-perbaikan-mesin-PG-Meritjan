7a5de6473805caf708aa0fd5695525111988f0b5
Frankie Steinlie <frankie.intern24slides@gmail.com>
2025-06-03 08:19:53 +0700

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,33 +53,6 @@ class AuthController extends Controller
             ]);
         }
 
-        // Perbaikan token API Fonnte
-        $token = "RWQHVXjZJS2nuH698t7C"; // Ganti dengan token API Fonnte yang valid
-        $target = "6287733560829";
-        $data = "Nama : " . Auth::user()->nama . "\n- NIP : " . Auth::user()->nip . "\n- Tanggal : " . date('d-m-Y H:i:s');
-
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.fonnte.com/send',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array(
-                'target' => $target,
-                'message' => $data,
-            ),
-            CURLOPT_HTTPHEADER => array(
-                "Authorization: $token"
-            ),
-        ));
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-
         $request->session()->regenerate();
         return redirect()->route('dashboard');
     }
